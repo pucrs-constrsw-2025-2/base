15dac950b6b2d0a95786e761a8a22bafa367d98d
Pedro Augusto Pereira <86989862+PedroAugustoPereira@users.noreply.github.com>
2025-08-18 23:47:37 +0000

    feat api oAuth config and all endpoints

---
+++ backend/oauth/src/main/java/com/grupo6/constrsw/dto/UserRequest.java
@@ -0,0 +1,30 @@
+package com.grupo6.constrsw.dto;
+
+public class UserRequest {
+    
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    
+    public UserRequest() {}
+    
+    public UserRequest(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
