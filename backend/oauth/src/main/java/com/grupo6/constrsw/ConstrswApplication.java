d91df7437bc40e6e44572a5e1faa525e7415381c
Gabriel Teiga <gabrielteiga99@gmail.com>
2025-08-18 19:41:39 -0300

    feat: adding default spring api

---
+++ backend/oauth/src/main/java/com/grupo6/constrsw/ConstrswApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.constrsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstrswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstrswApplication.class, args);
+	}
+
+}
