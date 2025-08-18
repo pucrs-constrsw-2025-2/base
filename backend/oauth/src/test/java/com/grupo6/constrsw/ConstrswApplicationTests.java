ae025941aa670f9510832816fbf55046c05187ca
Gabriel Teiga <gabrielteiga99@gmail.com>
2025-08-18 19:41:39 -0300

    feat: adding default spring api

---
+++ backend/oauth/src/test/java/com/grupo6/constrsw/ConstrswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.constrsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstrswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
