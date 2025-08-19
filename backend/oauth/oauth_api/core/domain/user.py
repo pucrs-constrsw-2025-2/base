2b4bce8688bcf4677fc8d5f6447b97798c5b7279
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-08-18 22:30:04 -0300

    fix: re-add email field to user schema, oopsies :hand_over_mouth: and corret suntax for 'enabled' field's default value

--- backend/oauth/oauth_api/core/domain/user.py
+++ backend/oauth/oauth_api/core/domain/user.py
@@ -4,7 +4,8 @@ from pydantic import BaseModel, EmailStr
 class User(BaseModel):
     id: str
     username: str
+    email: EmailStr
     first_name: str | None = None
     last_name: str | None = None
-    enabled: bool | True
+    enabled: bool = True    # significa que o usuario esta ativo??
     password: str | None = None
