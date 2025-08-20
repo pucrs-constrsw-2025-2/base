faa47a13895a9b97dbe25f180c63698e35124bd4
Stefano Carraro <stefanodpc29@gmail.com>
2025-08-20 14:34:13 -0300

    FIX(OAuth): ajustado endpoint create user para usar DTOs

--- backend/oauth/src/dtos/req/create_user_req.rs
+++ backend/oauth/src/dtos/req/create_user_req.rs
@@ -3,8 +3,11 @@ use serde::Serialize;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct CreateUserReq{
+    #[serde(alias="email", alias = "userName", alias="user_name", alias="user-name")]
     username: String,
+    password: String,
+    #[serde(alias="firstName", alias="first_name", alias="first-name")]
     first_name: String,
+    #[serde(alias="lastName", alias="last_name", alias="last-name")]
     last_name: String,
-    email: String
 }
