f3fe898494a7c0042f2d005633161412fd7fda6c
Stefano Carraro <stefanodpc29@gmail.com>
2025-08-20 17:43:39 -0300

    FEAT(OAuth): implementado endpoint create role

---
+++ backend/oauth/src/dtos/req/create_role_req.rs
@@ -0,0 +1,12 @@
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateRoleReq {
+    pub name: String,
+    pub composite: bool,
+    #[serde(alias="clientRole", alias="client_role", alias="client-role")]
+    pub client_role: bool,
+    #[serde(alias="containerId", alias="container_id", alias="container-id")]
+    pub container_id: String
+}
