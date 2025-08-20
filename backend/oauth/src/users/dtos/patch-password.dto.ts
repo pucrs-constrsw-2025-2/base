df9ef66a54b81a9927d04c2dbe05c98a7b3b4699
Gabriel <gabriel.petry08@gmail.com>
2025-08-20 20:19:05 -0300

    add swagger doc

--- backend/oauth/src/users/dtos/patch-password.dto.ts
+++ backend/oauth/src/users/dtos/patch-password.dto.ts
@@ -1,3 +1,7 @@
+
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PatchPasswordDto {
+  @ApiProperty({ example: 'novaSenha123', description: 'Nova senha do usuário' })
   password!: string;
 }
