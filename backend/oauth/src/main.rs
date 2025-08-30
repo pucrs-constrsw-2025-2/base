d8aea7db856795aaa5b32d755d3a441f4c2c2d76
Stefano Carraro <stefanodpc29@gmail.com>
2025-08-29 23:49:54 -0300

    FIX(OAuth): removed old update password code

--- backend/oauth/src/main.rs
+++ backend/oauth/src/main.rs
@@ -1,5 +1,5 @@
 use crate::adapters::web::controllers::general_controller::login_controller;
-use crate::adapters::web::controllers::user_controller::{create_user_controller, get_users_controller, get_user_controller, update_user_controller};
+use crate::adapters::web::controllers::user_controller::{create_user_controller, get_users_controller, get_user_controller, update_user_controller, update_password_controller};
 use actix_web::{App, HttpServer};
 use dotenv::dotenv;
 use std::env;
@@ -21,7 +21,7 @@ async fn main() -> std::io::Result<()> {
             .service(get_users_controller)
             .service(get_user_controller)
             .service(update_user_controller)
-            .service(patch_user_password)
+            .service(update_password_controller)
             .service(delete_user)
             .service(create_role)
             .service(get_role)
