068199102fa6e15117704806caf315f4edb1680c
Gabriel Teiga <gabrielteiga99@gmail.com>
2025-08-31 22:54:08 -0300

    feat: adding swagger into project

--- backend/oauth/src/main/java/com/grupo6/constrsw/dto/PasswordUpdateRequest.java
+++ backend/oauth/src/main/java/com/grupo6/constrsw/dto/PasswordUpdateRequest.java
@@ -1,7 +1,11 @@
 package com.grupo6.constrsw.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "DTO para requisição de atualização de senha")
 public class PasswordUpdateRequest {
     
+    @Schema(description = "Nova senha do usuário", example = "newPassword123", required = true)
     private String password;
     
     public PasswordUpdateRequest() {}
