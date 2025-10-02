d71e5677e35164ca9e7caa902b3d0d13f07b5ba7
Eduardo Arruda <13435770+eduardo-arruda@users.noreply.github.com>
2025-10-01 22:50:25 -0300

    Limpa frontend

--- frontend-temp/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  // Esta linha garante que todas as chamadas usem o prefixo correto
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
-});
-
-export default api;
