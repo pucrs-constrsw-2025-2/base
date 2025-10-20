b082380e5b273453a3edaa7ee6363d8dce9295b8
Lucasdpm <l.pozzo@edu.pucrs.br>
2025-10-20 18:56:09 -0300

    CHORE(backend): prepare to add 'backend/students' as a submodule

--- backend/students/Domain/Entities/PhoneNumber.cs
+++
@@ -1,9 +0,0 @@
-namespace Students.Domain.Entities
-{
-    public class PhoneNumber
-    {
-        public int Ddd { get; set; }
-        public int Number { get; set; }
-        public string Description { get; set; }
-    }
-}
